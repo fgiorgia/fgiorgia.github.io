34c2aac6a75158f96eea754a7b7f998c81dc3f32
Giorgia Faedda <giorgiafaedda3@gmail.com>
2024-07-17 20:51:20 +0200

    Add external fonts and scripts

--- next/packages/beautiful-next-theme/src/layouts/BaseLayout.tsx
+++ next/packages/beautiful-next-theme/src/layouts/BaseLayout.tsx
@@ -1,4 +1,3 @@
-import { getConfig } from "../config/getConfig"
 import React from "react"
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
@@ -23,6 +22,14 @@ const BaseLayout: React.FC<React.PropsWithChildren<Props>> = ({ children, config
   const { site } = config
   return (
     <html lang={page.language ?? site?.language ?? 'en'}>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" security="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" />
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" security="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" />
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" security="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" />
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" security="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" />
       <body>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
@@ -40,33 +47,6 @@ const BaseLayout: React.FC<React.PropsWithChildren<Props>> = ({ children, config
 
 // TODO: check how to load external fonts and scripts
 const frontMatter = {
-  "common-css": [
-    "/assets/css/bootstrap-social.css",
-    "/assets/css/beautifuljekyll.css"
-  ],
-  "common-ext-css": [
-    {
-      "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
-      "sri": "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-    },
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css",
-    "https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic",
-    "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
-  ],
-  "common-ext-js": [
-    {
-      "href": "https://code.jquery.com/jquery-3.5.1.slim.min.js",
-      "sri": "sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
-    },
-    {
-      "href": "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
-      "sri": "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    },
-    {
-      "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js",
-      "sri": "sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-    }
-  ],
   "common-js": [
     "/assets/js/beautifuljekyll.js"
   ]
