9b1be98802f70435b7418b5a188e7b14b411502a
Giorgia Faedda <giorgiafaedda3@gmail.com>
2024-09-21 11:47:06 +0200

    Add links and refactor

--- next/packages/fgiorgia.github.io/src/app/about/page.tsx
+++ next/packages/fgiorgia.github.io/src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page: React.FC = () => {
-  return <h1>Hello, Project Page!</h1>
+  return <h1>Hello, About Page!</h1>
 };
 
 export default Page;
--- next/packages/fgiorgia.github.io/src/components/Header/Header.tsx
+++ next/packages/fgiorgia.github.io/src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
+import { classNames } from "@fgiorgia-site/utils/style/classNames"
 import Stack from "../Stack/Stack"
 import Avatar from "./Avatar"
+import Link from "next/link"
+
+const linkClasses = 'font-extrabold text-lg'
+
+const HeaderLink: React.FC<{ children: string, href: string }> = ({ href, children }) => {
+  return (
+    <Link className='font-extrabold text-lg' href={href}>{children}</Link>
+  )
+}
 
 const Header: React.FC = () => {
   return (
@@ -7,16 +17,16 @@ const Header: React.FC = () => {
       <Stack direction="row" className="w-full min-h-20 bg-gray-100 px-4">
         {/* Left side */}
         <Stack className="flex-row justify-center items-center">
-          <p>Giorgia Faedda</p>
+          <HeaderLink href="/">Giorgia Faedda</HeaderLink>
         </Stack>
         <Stack className="flex-1 relative justify-center">
           <Avatar />
         </Stack>
         {/* Right side */}
         <Stack direction="row" className="justify-center items-center gap-2">
-          <p>Projects</p>
-          <p>GitHub</p>
-          <p>About Me</p>
+          <HeaderLink href="/projects">Projects</HeaderLink>
+          <HeaderLink href="https://github.com/fgiorgia">GitHub</HeaderLink>
+          <HeaderLink href="/about">About Me</HeaderLink>
         </Stack>
       </Stack>
     </header>
