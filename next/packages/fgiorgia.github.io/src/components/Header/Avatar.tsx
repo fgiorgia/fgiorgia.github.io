90def17ce8073116006a07fd87f1789c6f4c349b
Giorgia Faedda <giorgiafaedda3@gmail.com>
2024-09-21 14:16:10 +0200

    Make avatar image prioritary

--- next/packages/fgiorgia.github.io/src/components/Header/Avatar.tsx
+++ next/packages/fgiorgia.github.io/src/components/Header/Avatar.tsx
@@ -3,8 +3,14 @@ import avatar from "./avatar.jpeg";
 
 const Avatar: React.FC = () => {
   return (
-    <Image src={avatar} height={100} alt="Avatar"
-      className="avatar absolute -bottom-12 rounded-full z-10" style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.5))' }} />);
+    <Image
+      priority={true}
+      src={avatar}
+      height={100}
+      alt="Avatar"
+      className="avatar absolute -bottom-12 rounded-full z-10"
+      style={{ filter: 'drop-shadow(0 1px 2px rgba(0, 0, 0, 0.5))' }}
+    />);
 }
 
 export default Avatar;
