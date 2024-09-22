f9cdeafeefe70047605ea3b583e9556006032995
Giorgia Faedda <giorgiafaedda3@gmail.com>
2024-09-22 11:28:22 +0200

    Fix Carousel overlay

--- next/packages/fgiorgia.github.io/src/components/Carousel/Carousel.tsx
+++ next/packages/fgiorgia.github.io/src/components/Carousel/Carousel.tsx
@@ -32,7 +32,7 @@ const Carousel = () => {
 
   return (
     <Stack className='justify-center'>
-      <Stack style={{ height: '32rem', width: '100%', position: 'relative' }}>
+      <Stack style={{ height: '32rem', width: '100%', position: 'relative', overflowX: 'hidden' }}>
         <Stack style={{ height: '100%', width: '200%', position: 'absolute' }}>
           {indeces.map((i) => {
             return (
