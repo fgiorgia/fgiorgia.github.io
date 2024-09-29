77e1f4275ced942b6ecc0cc0bb24eea80cd65160
Giorgia Faedda <giorgiafaedda3@gmail.com>
2024-09-29 14:51:04 +0200

    Make nav responsive

--- next/packages/fgiorgia.github.io/src/components/Header/Header.tsx
+++ next/packages/fgiorgia.github.io/src/components/Header/Header.tsx
@@ -2,20 +2,7 @@ import { classNames } from "@/utils/style/classNames"
 import Stack from "../Stack/Stack"
 import Avatar from "./Avatar"
 import Link from "next/link"
-
-const linkClasses = 'font-extrabold text-gray-800 hover:text-cyan-800'
-
-const HeaderLink: React.FC<{ children: string, href: string, openInNewTab?: boolean }> = ({ href, children, openInNewTab }) => {
-  return (
-    <Link
-      className={classNames(linkClasses, 'text-sm uppercase')}
-      href={href}
-      target={openInNewTab === true ? '_blank' : undefined}
-    >
-      {children}
-    </Link>
-  )
-}
+import NavbarLinkArea, { linkClasses } from "./NavbarLinkArea"
 
 const Header: React.FC = () => {
   return (
@@ -29,12 +16,7 @@ const Header: React.FC = () => {
           <Avatar />
         </Stack>
         {/* Right side */}
-        <Stack direction="row" className="w-1/3 justify-center items-center gap-8">
-          <HeaderLink href="/projects">Projects</HeaderLink>
-          <HeaderLink href="https://github.com/fgiorgia" openInNewTab>GitHub</HeaderLink>
-          <HeaderLink href="/about">About Me</HeaderLink>
-          {/* TODO: Add search button */}
-        </Stack>
+        <NavbarLinkArea />
       </Stack>
     </header>
   )
