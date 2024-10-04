9b1491dffb1d7d45e59656ca14a081507390c9c3
Giorgia Faedda <giorgiafaedda3@gmail.com>
2024-10-04 21:06:37 +0200

    Remove unused old packages

--- packages/beautiful-next-based-fgiorgia.github.io/next.config.mjs
+++
@@ -1,8 +0,0 @@
-import nextMDX from '@next/mdx'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-const nextConfigWithMDX = nextMDX()(nextConfig)
-
-export default nextConfigWithMDX;
--- packages/beautiful-next-theme/src/utils/links.ts
+++
@@ -1,9 +0,0 @@
-// Implemented as identity, may be needed later
-export const relativeUrl = (url: string): string => {
-  return url
-}
-
-// Implemented as identity, may be needed later
-export const absoluteUrl = (url: string): string => {
-  return url
-}
