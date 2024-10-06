a84c23a4cba9275c05ac802110d30997c07ed792
Giorgia Faedda <giorgiafaedda3@gmail.com>
2024-10-06 18:43:16 +0200

    Add prettier and prettier rules

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * Format style rules
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  experimentalTernaries: true,
+};
+
+export default config;
