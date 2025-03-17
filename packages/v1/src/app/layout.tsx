944500fd86041fb2d1d5ab2aac00bf48e0348a8f
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-03-17 12:08:42 +0100

    Change cloudflare setting

--- packages/v1/src/app/layout.tsx
+++ packages/v1/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
-          rel="stylesheet"
+          rel="preconnect"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
           integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
           crossOrigin="anonymous"
