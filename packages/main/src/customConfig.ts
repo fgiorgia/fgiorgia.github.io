2742a0f66d978cf7cfa169b052b1c4389f78f21e
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-09-13 18:54:59 +0200

    Adopt swiftpost-next-static-template

---
+++ packages/main/src/customConfig.ts
@@ -0,0 +1,8 @@
+import { CustomConfig } from './types';
+
+export const customConfig: CustomConfig = {
+  author: {
+    name: 'Giorgia Faedda',
+    email: 'giorgia.faedda@gfanalytics.se',
+  },
+};
