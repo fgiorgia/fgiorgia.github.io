fbaa2aeaa1b0e76718af42e9ad5d224a2a77f1f2
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-09-13 20:50:35 +0200

    Remove unused @swiftpost-ui

--- packages/@swiftpost-ui/src/core/Stack.tsx
+++
@@ -1,8 +0,0 @@
-import { Stack as MUIStack } from '@mui/material'
-import { type PropsWithChildren } from 'react'
-
-const Stack: React.FC<PropsWithChildren> = ({ children }) => {
-  return <MUIStack>{children}</MUIStack>
-}
-
-export default Stack
--- packages/@swiftpost-ui/src/layout/Section.tsx
+++
@@ -1,8 +0,0 @@
-import Stack from '@/core/Stack'
-import { type PropsWithChildren } from 'react'
-
-const Section: React.FC<PropsWithChildren> = ({ children }) => {
-  return <Stack>{children}</Stack>
-}
-
-export default Section
