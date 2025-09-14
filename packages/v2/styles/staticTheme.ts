0b6fb3d2f0b92f35fcda46d4995fe5fce3629f12
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-09-14 20:39:10 +0200

    Override theme breakpoints

--- packages/v2/styles/staticTheme.ts
+++ packages/v2/styles/staticTheme.ts
@@ -1,3 +1,17 @@
 import { staticThemeOptions } from '@swiftpost/elysium/themes/gamut/static';
 
-export const staticTheme = staticThemeOptions;
+const breakpoints = {
+  values: {
+    ...staticThemeOptions.breakpoints.values,
+    xs: 340,
+    sm: 600,
+    md: 900,
+    lg: 1280,
+    xl: 1536,
+  },
+} as const;
+
+export const staticTheme = {
+  ...staticThemeOptions,
+  breakpoints,
+};
