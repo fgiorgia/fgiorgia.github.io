917e48a28e29015a362d091747ac9c797e0b7d3a
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-09-17 08:41:37 +0200

    Add fontWeights to static theme

--- packages/v2/styles/staticTheme.ts
+++ packages/v2/styles/staticTheme.ts
@@ -15,3 +15,17 @@ export const staticTheme = {
   ...staticThemeOptions,
   breakpoints,
 };
+
+export const fontWeights = {
+  thin: 100,
+  extraLight: 200,
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+  extraBold: 800,
+  black: 900,
+} as const;
+
+export const unit = staticTheme.spacing;
