d0b48458fae3c5b3ed59e0bb4bcd9c865edeb28e
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-09-17 08:42:23 +0200

    Create initial extended base theme for MUI using Inter font

--- packages/v2/styles/theme.ts
+++ packages/v2/styles/theme.ts
@@ -1,1 +1,20 @@
-export { theme, mainFont } from '@swiftpost/elysium/themes/gamut';
+'use client';
+import { createTheme, type ThemeOptions } from '@mui/material/styles';
+import { theme as baseTheme } from '@swiftpost/elysium/themes/gamut';
+
+export const theme = createTheme(baseTheme as ThemeOptions, {
+  fontFamily: {
+    sans: [
+      'Inter',
+      'ui-sans-serif',
+      'system-ui',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Roboto',
+      'Helvetica Neue',
+      'Arial',
+      'sans-serif',
+    ],
+  },
+});
