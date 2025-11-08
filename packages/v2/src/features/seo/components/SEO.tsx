8a53347a6cde90d6a94eaba34aef27a6aab553b7
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-11-08 14:13:40 +0100

    Disable part of SEO component using next/head in App Router

--- packages/v2/src/features/seo/components/SEO.tsx
+++ packages/v2/src/features/seo/components/SEO.tsx
@@ -1,9 +1,10 @@
-// packages/v2/components/seo/SEO.tsx
 import React from 'react';
 import { NextSeo, ArticleJsonLd } from 'next-seo';
 import { getAssetPath } from '@/core/common/utils';
 import { SEOProps } from '@/core/common/types';
 import { siteUrl, defaultSEO } from '../seo';
+import { useIsAppRouter } from '@/core/compat/isAppRouter';
+import AppRouterSEO from './AppRouterSEO';
 
 const SEO: React.FC<SEOProps> = ({
   title,
@@ -22,36 +23,41 @@ const SEO: React.FC<SEOProps> = ({
       : `${siteUrl}${getAssetPath(ogImage)}`
     : `${siteUrl}${getAssetPath('./images/gfanalytics-og.png')}`;
 
+  const isAppRouter = useIsAppRouter();
+
   return (
     <>
-      <NextSeo
-        title={title}
-        description={description}
-        canonical={pageUrl}
-        noindex={noindex}
-        titleTemplate={defaultSEO.titleTemplate} // Explicitly use the titleTemplate from defaultSEO
-        openGraph={{
-          url: pageUrl,
-          title: title, // This will be used as-is for OpenGraph
-          description: description,
-          images: [
-            {
-              url: imageUrl,
-              width: 1200,
-              height: 630,
-              alt: ogImageAlt || title || 'Page image',
-            },
-          ],
-          ...(article && {
-            type: 'article',
-            article: {
-              publishedTime: article.publishedTime,
-              modifiedTime: article.modifiedTime,
-              tags: article.tags,
-            },
-          }),
-        }}
-      />
+      {isAppRouter ?
+        <AppRouterSEO />
+      : <NextSeo
+          title={title}
+          description={description}
+          canonical={pageUrl}
+          noindex={noindex}
+          titleTemplate={defaultSEO.titleTemplate} // Explicitly use the titleTemplate from defaultSEO
+          openGraph={{
+            url: pageUrl,
+            title: title, // This will be used as-is for OpenGraph
+            description: description,
+            images: [
+              {
+                url: imageUrl,
+                width: 1200,
+                height: 630,
+                alt: ogImageAlt || title || 'Page image',
+              },
+            ],
+            ...(article && {
+              type: 'article',
+              article: {
+                publishedTime: article.publishedTime,
+                modifiedTime: article.modifiedTime,
+                tags: article.tags,
+              },
+            }),
+          }}
+        />
+      }
       {article && (
         <ArticleJsonLd
           type="BlogPosting"
